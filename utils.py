e2e66a73a40c50b5bab0ca9c531d12ad20e58571
Maxxxxxon <m.faleev@innopolis.university>
2021-10-02 17:17:34 +0300

    + moving average class

--- utils.py
+++ utils.py
@@ -1,5 +1,22 @@
 import pandas as pd
 
+
+class MovingAverage:
+    def __init__(self, momentum, initial_value=0):
+        assert 0.0 < momentum <= 1.0
+        self.momentum = momentum
+        self.moving_average = initial_value
+        
+    def add(self, new_value):
+        self.moving_average = self.moving_average * (1 - self.momentum) + new_value * self.momentum
+    
+    def value(self,):
+        return self.moving_average
+    
+    def __str__(self,):
+        return str(self.moving_average)
+    
+    
 def convert_ids_to_ordered(users: pd.DataFrame, organizations: pd.DataFrame, reviews: pd.DataFrame) -> tuple:
     ordered_users = users.copy()
     ordered_users['ordered_id'] = range(len(ordered_users)) 
